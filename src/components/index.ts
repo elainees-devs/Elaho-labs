9a323120729cd7e65d8c0faf7b9986d4b9a8327b
elainees-devs <elainees.devs@gmail.com>
2026-03-15 20:10:28 +0300

    feat: export all layout and section components from index

---
+++ src/components/index.ts
@@ -0,0 +1,12 @@
+// Export all components from layout
+export { default as Navbar } from './layout/Navbar';
+export { default as Footer } from './layout/Footer';
+
+// Export all components from sections
+export { default as Hero } from './sections/Hero';
+export { default as Services } from './sections/Services';
+export { default as Projects } from './sections/Projects';
+export { default as CTA } from './sections/CTA';
+export { default as Contact } from './sections/Contact';
+export { default as About } from './sections/About';
+export { default as WhyChooseElaho } from './sections/WhyChooseElaho';
