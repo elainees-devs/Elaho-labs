a694f9d0395aa81da4f8a9ed8409bcb68c0b224d
elainees-devs <elainees.devs@gmail.com>
2026-03-15 19:58:54 +0300

    feat: add Footer component with copyright notice

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,1 +1,12 @@
 // Footer component
+import React from 'react';
+
+const Footer: React.FC = () => {
+  return (
+    <footer className="py-10 text-center text-gray-500">
+      © {new Date().getFullYear()} Elaho Labs. All rights reserved.
+    </footer>
+  );
+};
+
+export default Footer;
