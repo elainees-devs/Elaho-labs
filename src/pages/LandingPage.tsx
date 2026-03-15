f7ff4fcc3d2fb250a5e9cdcbe54476a0de4d3fa9
elainees-devs <elainees.devs@gmail.com>
2026-03-15 19:55:39 +0300

    feat: add LandingPage component with all main sections

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,1 +1,30 @@
-// Landing page component
+import React from "react";
+import {
+  Navbar,
+  Hero,
+  Services,
+  Projects,
+  CTA,
+  About,
+  WhyChooseElaho,
+  Contact,
+  Footer,
+} from "../components";
+
+const LandingPage: React.FC = () => {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Services />
+      <Projects />
+      <WhyChooseElaho />
+      <CTA />
+      <About />
+      <Contact />
+      <Footer />
+    </>
+  );
+};
+
+export default LandingPage;
